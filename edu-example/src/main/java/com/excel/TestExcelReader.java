2bd2e9d5bb8184dd0e6f3c9c9ada62b1fdcb9105
hui2.chen <hui2.chen@ly.com>
2019-07-10 11:52:36 +0800

    Excel读取 大数据量

--- edu-example/src/main/java/com/excel/TestExcelReader.java
+++ edu-example/src/main/java/com/excel/TestExcelReader.java
@@ -1,6 +1,7 @@
 package com.excel;
 
 import com.monitorjbl.xlsx.StreamingReader;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -69,5 +70,9 @@ public class TestExcelReader {
 //        System.gc();
 //        Thread.sleep(Integer.MAX_VALUE);
 
+    }
+
+    private String getCellValue(Cell cell) {
+        return cell.getStringCellValue();
     }
 }
