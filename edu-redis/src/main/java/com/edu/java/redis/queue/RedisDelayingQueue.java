491fceb44e6ef1c34b6ffedab1630661727d5460
hui2.chen <hui2.chen@ly.com>
2020-06-09 00:55:31 +0800

    redis 实现延迟队列

--- edu-redis/src/main/java/com/edu/java/redis/queue/RedisDelayingQueue.java
+++ edu-redis/src/main/java/com/edu/java/redis/queue/RedisDelayingQueue.java
@@ -1,11 +1,68 @@
 package com.edu.java.redis.queue;
 
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import redis.clients.jedis.Jedis;
+
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * 2. redis的延迟队列
  * 通过Redis的 zset(有序列表) 来实现
- * @param <T>
+ *
+ * @author huichen
+ * @date 2020-06
  */
 public class RedisDelayingQueue<T> {
 
+    static class TaskItem<T> {
+        public String id;
+        public T msg;
+    }
 
+    private Type TaskType = new TypeReference<TaskItem<T>>() {
+    }.getType();
+
+    private Jedis jedis;
+    private String queueKey;
+
+    public RedisDelayingQueue(Jedis jedis, String queueKey) {
+        this.jedis = jedis;
+        this.queueKey = queueKey;
+    }
+
+    public void delay(T msg) {
+        TaskItem task = new TaskItem<T>();
+        task.id = UUID.randomUUID().toString();
+        task.msg = msg;
+        String s = JSON.toJSONString(task);
+        jedis.zadd(queueKey, System.currentTimeMillis() + 5000, s);
+    }
+
+    public void loop() {
+        while (!Thread.interrupted()) {
+            Set<String> values = jedis.zrangeByScore(queueKey, 0, System.currentTimeMillis(), 0, 1);
+            if (values.isEmpty()) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    break;
+                }
+                continue;
+            }
+            String s = values.iterator().next();
+            if (jedis.zrem(queueKey, s) > 0) {
+
+                TaskItem<T> task = JSON.parseObject(s, TaskType);
+                this.handleMsg(task.msg);
+            }
+        }
+    }
+
+    private void handleMsg(T msg) {
+        System.out.println(msg);
+    }
 }
---
+++ edu-redis/src/test/java/com/edu/java/redis/queue/RedisDelayingQueueTest.java
@@ -0,0 +1,41 @@
+package com.edu.java.redis.queue;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+public class RedisDelayingQueueTest {
+
+    @Test
+    public void delay() {
+        Jedis jedis = new Jedis();
+        RedisDelayingQueue<String> queue = new RedisDelayingQueue<>(jedis, "q-demo");
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    queue.delay("codehole" + i);
+                }
+            }
+        });
+
+
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                queue.loop();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        try {
+            producer.join();
+            Thread.sleep(6000);
+            consumer.interrupt();
+            consumer.join();
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
